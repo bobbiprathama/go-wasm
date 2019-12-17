4cf2e5fc63035bc6ac487a9c6aedd5f0df17f1cc
bobbiprathama <bobbiprathama@gmail.com>
2019-12-17 12:23:07 +0700

    format print only

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello, WebAssembly!")
-	log.Println("Hello, WebAssembly!")
 
 	http.ListenAndServe(`:8080`, http.FileServer(http.Dir(`.`)))
 }
